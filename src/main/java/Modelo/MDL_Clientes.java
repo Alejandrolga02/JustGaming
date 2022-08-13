796023f06d506e7f1867b9cc98b040cfa8961dfa
JesusDaniel117 <2020030232@upsin.edu.mx>
2022-08-13 17:22:12 -0600

    Cliente

--- src/main/java/Modelo/MDL_Clientes.java
+++ src/main/java/Modelo/MDL_Clientes.java
@@ -5,13 +5,38 @@ Fecha: 13-agosto-2022
 */
 package Modelo;
 
-/**
- *
- * @author laptop
- */
+import static Modelo.Conexion.getConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MDL_Clientes {
     //Atributos de la clase
+    Connection conn = null;
     
     //Método para insertar en la tabla clientes.
+    public boolean clientesInsertar(String nombreCompleto, String telefono, String correoElectronico, 
+                                    String domicilio){
+        try{
+            //Abrir la conexion
+            conn = getConnection();
+            //Preparando la instruccion
+            String sql = "INSERT INTO cliente (nombreCompleto,telefono,correoElectronico,domicilio) VALUES ('" + nombreCompleto + "','" + 
+                    telefono + "','" + correoElectronico + "','" + domicilio + "');";
+            //Ejecucución de la sentencia
+            Statement stm = conn.createStatement();
+            int registro = stm.executeUpdate(sql); //Los Update regresan valores enteros
+            //Cierre de la conexión
+            Conexion.close(conn);
+            return true;
+        }catch(SQLException ex){
+            return false;
+        }
+        
+    }
     
+    //Método para Eliminar a un cliente
+    public boolean clinteEliminar(){
+        return false;
+    }
 }
